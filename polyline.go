2da4e8b725f1b05a8518513d9f0ff97641c3d36a
Tom Payne <twpayne@gmail.com>
2015-11-26 20:54:44 +0100

    Rename result to buf to better reflect purpose

--- polyline.go
+++ polyline.go
@@ -23,50 +23,50 @@ type Codec struct {
 
 var defaultCodec = Codec{Dim: 2, Scale: 1e5}
 
-// EncodeUint appends the encoding of a single unsigned integer u to result.
-func EncodeUint(u uint, result []byte) []byte {
+// EncodeUint appends the encoding of a single unsigned integer u to buf.
+func EncodeUint(u uint, buf []byte) []byte {
 	for u >= 32 {
-		result = append(result, byte((u&31)+95))
+		buf = append(buf, byte((u&31)+95))
 		u = u >> 5
 	}
-	result = append(result, byte(u+63))
-	return result
+	buf = append(buf, byte(u+63))
+	return buf
 }
 
-// EncodeInt appends the encoding of a single signed integer i to result.
-func EncodeInt(i int, result []byte) []byte {
+// EncodeInt appends the encoding of a single signed integer i to buf.
+func EncodeInt(i int, buf []byte) []byte {
 	var u uint
 	if i < 0 {
 		u = uint(^(i << 1))
 	} else {
 		u = uint(i << 1)
 	}
-	return EncodeUint(u, result)
+	return EncodeUint(u, buf)
 }
 
-// EncodeFloat64 appends the encoding of a single float64 f to result.
-func (c Codec) EncodeFloat64(f float64, result []byte) []byte {
-	return EncodeInt(int(round(c.Scale*f)), result)
+// EncodeFloat64 appends the encoding of a single float64 f to buf.
+func (c Codec) EncodeFloat64(f float64, buf []byte) []byte {
+	return EncodeInt(int(round(c.Scale*f)), buf)
 }
 
-// EncodeCoords appends the encoding of an array of coordinates coords to result.
-func (c Codec) EncodeCoords(coords [][]float64, result []byte) []byte {
+// EncodeCoords appends the encoding of an array of coordinates coords to buf.
+func (c Codec) EncodeCoords(coords [][]float64, buf []byte) []byte {
 	last := make([]float64, c.Dim)
 	for _, coord := range coords {
 		for i, x := range coord {
-			result = c.EncodeFloat64(x-last[i], result)
+			buf = c.EncodeFloat64(x-last[i], buf)
 			last[i] = x
 		}
 	}
-	return result
+	return buf
 }
 
-// EncodeFloat64 appends the encoding of a single float64 f to result.
-func EncodeFloat64(f float64, result []byte) []byte {
-	return defaultCodec.EncodeFloat64(f, result)
+// EncodeFloat64 appends the encoding of a single float64 f to buf.
+func EncodeFloat64(f float64, buf []byte) []byte {
+	return defaultCodec.EncodeFloat64(f, buf)
 }
 
-// EncodeCoords appends the encoding of an array of coordinates coords to result.
-func EncodeCoords(coords [][]float64, result []byte) []byte {
-	return defaultCodec.EncodeCoords(coords, result)
+// EncodeCoords appends the encoding of an array of coordinates coords to buf.
+func EncodeCoords(coords [][]float64, buf []byte) []byte {
+	return defaultCodec.EncodeCoords(coords, buf)
 }
