aa521810be295e53944dbfa103b5525991a060b1
Tom Payne <twpayne@gmail.com>
2015-11-26 20:25:40 +0100

    Add some documentation

--- polyline/polyline.go
+++ polyline/polyline.go
@@ -1,3 +1,6 @@
+// Package polyline implements a Google Maps Encoding Polyline encoder.
+//
+// See https://developers.google.com/maps/documentation/utilities/polylinealgorithm
 package polyline
 
 import (
@@ -12,6 +15,7 @@ func round(x float64) float64 {
 	}
 }
 
+// EncodeUint appends the encoding of a single unsigned integer u to result.
 func EncodeUint(u uint, result []byte) []byte {
 	for u >= 32 {
 		result = append(result, byte((u&31)+95))
@@ -21,6 +25,7 @@ func EncodeUint(u uint, result []byte) []byte {
 	return result
 }
 
+// EncodeInt appends the encoding of a single signed integer i to result.
 func EncodeInt(i int, result []byte) []byte {
 	var u uint
 	if i < 0 {
@@ -31,10 +36,12 @@ func EncodeInt(i int, result []byte) []byte {
 	return EncodeUint(u, result)
 }
 
+// EncodeFloat64 appends the encoding of a single float64 f to result.
 func EncodeFloat64(f float64, result []byte) []byte {
 	return EncodeInt(int(round(1e5*f)), result)
 }
 
+// EncodeCoords appends the encoding of an array of coordinates coords to result.
 func EncodeCoords(coords [][]float64, result []byte) []byte {
 	lastLat, lastLong := 0, 0
 	for _, coord := range coords {
