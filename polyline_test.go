272fd734e543d253d77c152ec4776b4e1f4ac7e1
Tom Payne <twpayne@gmail.com>
2015-11-27 16:43:34 +0100

    Rename variables

--- polyline_test.go
+++ polyline_test.go
@@ -24,7 +24,7 @@ func ExampleDecodeCoords() {
 }
 
 func TestUint(t *testing.T) {
-	for _, c := range []struct {
+	for _, tc := range []struct {
 		u uint
 		s string
 	}{
@@ -33,17 +33,17 @@ func TestUint(t *testing.T) {
 		{u: 32, s: "_@"},
 		{u: 174, s: "mD"},
 	} {
-		if got, b, err := DecodeUint([]byte(c.s)); got != c.u || len(b) != 0 || err != nil {
-			t.Errorf("DecodeUint(%v) = %v, %v, %v, want %v, nil, nil", c.s, got, err, string(b), c.u)
+		if got, b, err := DecodeUint([]byte(tc.s)); got != tc.u || len(b) != 0 || err != nil {
+			t.Errorf("DecodeUint(%v) = %v, %v, %v, want %v, nil, nil", tc.s, got, err, string(b), tc.u)
 		}
-		if got := EncodeUint(c.u, nil); string(got) != c.s {
-			t.Errorf("EncodeUint(%v) = %v, want %v", c.u, string(got), c.s)
+		if got := EncodeUint(tc.u, nil); string(got) != tc.s {
+			t.Errorf("EncodeUint(%v) = %v, want %v", tc.u, string(got), tc.s)
 		}
 	}
 }
 
 func TestInt(t *testing.T) {
-	for _, c := range []struct {
+	for _, tc := range []struct {
 		i int
 		s string
 	}{
@@ -55,17 +55,17 @@ func TestInt(t *testing.T) {
 		{i: 255200, s: "_mqN"},
 		{i: -550300, s: "vxq`@"},
 	} {
-		if got, b, err := DecodeInt([]byte(c.s)); got != c.i || len(b) != 0 || err != nil {
-			t.Errorf("DecodeInt(%v) = %v, %v, %v, want %v, nil, nil", c.s, got, err, string(b), c.i)
+		if got, b, err := DecodeInt([]byte(tc.s)); got != tc.i || len(b) != 0 || err != nil {
+			t.Errorf("DecodeInt(%v) = %v, %v, %v, want %v, nil, nil", tc.s, got, err, string(b), tc.i)
 		}
-		if got := EncodeInt(c.i, nil); string(got) != c.s {
-			t.Errorf("EncodeInt(%v) = %v, want %v", c.i, string(got), c.s)
+		if got := EncodeInt(tc.i, nil); string(got) != tc.s {
+			t.Errorf("EncodeInt(%v) = %v, want %v", tc.i, string(got), tc.s)
 		}
 	}
 }
 
 func TestCoord(t *testing.T) {
-	for _, c := range []struct {
+	for _, tc := range []struct {
 		s string
 		c []float64
 	}{
@@ -74,17 +74,17 @@ func TestCoord(t *testing.T) {
 			c: []float64{38.5, -120.2},
 		},
 	} {
-		if got, b, err := DecodeCoord([]byte(c.s)); !reflect.DeepEqual(got, c.c) || len(b) != 0 || err != nil {
-			t.Errorf("DecodeCoord(%v) = %v, %v, %v, want %v, nil, nil", c.s, got, err, string(b), c.c)
+		if got, b, err := DecodeCoord([]byte(tc.s)); !reflect.DeepEqual(got, tc.c) || len(b) != 0 || err != nil {
+			t.Errorf("DecodeCoord(%v) = %v, %v, %v, want %v, nil, nil", tc.s, got, err, string(b), tc.c)
 		}
-		if got := EncodeCoord(c.c, nil); string(got) != c.s {
-			t.Errorf("EncodeCoord(%v, nil) = %v, want %v", c.c, got, string(got), c.s)
+		if got := EncodeCoord(tc.c, nil); string(got) != tc.s {
+			t.Errorf("EncodeCoord(%v, nil) = %v, want %v", tc.c, got, string(got), tc.s)
 		}
 	}
 }
 
 func TestCoords(t *testing.T) {
-	for _, c := range []struct {
+	for _, tc := range []struct {
 		cs [][]float64
 		s  string
 	}{
@@ -93,11 +93,11 @@ func TestCoords(t *testing.T) {
 			s:  "_p~iF~ps|U_ulLnnqC_mqNvxq`@",
 		},
 	} {
-		if got, b, err := DecodeCoords([]byte(c.s)); !reflect.DeepEqual(got, c.cs) || len(b) != 0 || err != nil {
-			t.Errorf("DecodeCoords(%v) = %v, %v, %v, want %v, nil, nil", c.s, got, string(b), err, c.cs)
+		if got, b, err := DecodeCoords([]byte(tc.s)); !reflect.DeepEqual(got, tc.cs) || len(b) != 0 || err != nil {
+			t.Errorf("DecodeCoords(%v) = %v, %v, %v, want %v, nil, nil", tc.s, got, string(b), err, tc.cs)
 		}
-		if got := EncodeCoords(c.cs, nil); string(got) != c.s {
-			t.Errorf("EncodeCoords(%v) = %v, want %v", c.cs, string(got), c.s)
+		if got := EncodeCoords(tc.cs, nil); string(got) != tc.s {
+			t.Errorf("EncodeCoords(%v) = %v, want %v", tc.cs, string(got), tc.s)
 		}
 	}
 }
