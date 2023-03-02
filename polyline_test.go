ce17a2e7cc4e33f8d3fe1e2e5775cb7dfd3f9c11
Tom Payne <tom.payne@flarm.com>
2023-03-02 22:54:40 +0100

    Improve use of alecthomas/assert

--- polyline_test.go
+++ polyline_test.go
@@ -32,7 +32,7 @@ func TestUint(t *testing.T) {
 			got, b, err := polyline.DecodeUint([]byte(tc.s))
 			assert.NoError(t, err)
 			assert.Equal(t, tc.u, got)
-			assert.Equal(t, 0, len(b))
+			assert.Zero(t, len(b))
 			if !tc.nonCanonical {
 				assert.Equal(t, []byte(tc.s), polyline.EncodeUint(nil, tc.u))
 			}
@@ -110,7 +110,7 @@ func TestInt(t *testing.T) {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			got, b, err := polyline.DecodeInt([]byte(tc.s))
 			assert.NoError(t, err)
-			assert.Equal(t, 0, len(b))
+			assert.Zero(t, len(b))
 			assert.Equal(t, tc.i, got)
 			assert.Equal(t, []byte(tc.s), polyline.EncodeInt(nil, tc.i))
 		})
@@ -136,7 +136,7 @@ func TestCoord(t *testing.T) {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			got, b, err := polyline.DecodeCoord([]byte(tc.s))
 			assert.NoError(t, err)
-			assert.Equal(t, 0, len(b))
+			assert.Zero(t, len(b))
 			assert.Equal(t, tc.c, got)
 			if !tc.nonCanonical {
 				assert.Equal(t, []byte(tc.s), polyline.EncodeCoord(tc.c))
@@ -158,7 +158,7 @@ func TestCoords(t *testing.T) {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			got, b, err := polyline.DecodeCoords([]byte(tc.s))
 			assert.NoError(t, err)
-			assert.Equal(t, 0, len(b))
+			assert.Zero(t, len(b))
 			assert.Equal(t, tc.cs, got)
 			assert.Equal(t, []byte(tc.s), polyline.EncodeCoords(tc.cs))
 		})
@@ -179,7 +179,7 @@ func TestFlatCoords(t *testing.T) {
 			codec := polyline.Codec{Dim: 2, Scale: 1e5}
 			gotFCS, b, err := codec.DecodeFlatCoords(nil, []byte(tc.s))
 			assert.NoError(t, err)
-			assert.Equal(t, 0, len(b))
+			assert.Zero(t, len(b))
 			assert.Equal(t, tc.fcs, gotFCS)
 			gotBytes, err := codec.EncodeFlatCoords(nil, tc.fcs)
 			assert.NoError(t, err)
@@ -192,11 +192,11 @@ func TestFlatCoordsEmpty(t *testing.T) {
 	codec := polyline.Codec{Dim: 2, Scale: 1e5}
 	gotFCS, b, err := codec.DecodeFlatCoords(nil, nil)
 	assert.NoError(t, err)
-	assert.Equal(t, 0, len(b))
-	assert.Equal(t, 0, len(gotFCS))
+	assert.Zero(t, len(b))
+	assert.Zero(t, len(gotFCS))
 	gotBytes, err := codec.EncodeFlatCoords(nil, nil)
 	assert.NoError(t, err)
-	assert.Equal(t, 0, len(gotBytes))
+	assert.Zero(t, len(gotBytes))
 }
 
 func TestDecodeFlatCoordsErrors(t *testing.T) {
@@ -266,7 +266,7 @@ func TestCodec(t *testing.T) {
 			got, b, err := tc.c.DecodeCoords([]byte(tc.s))
 			assert.NoError(t, err)
 			assert.Equal(t, tc.cs, got)
-			assert.Equal(t, 0, len(b))
+			assert.Zero(t, len(b))
 			assert.Equal(t, []byte(tc.s), tc.c.EncodeCoords(nil, tc.cs))
 		})
 	}
